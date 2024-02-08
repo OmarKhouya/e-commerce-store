8289de70d104f60e3d6e220a26cafa4fb0a6070c
Omar ka <omarkhouya66@gmail.com>
2024-02-08 13:30:59 +0100

    added comments to indexs

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store";
 
+// Creating a root using ReactDOM.createRoot
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Rendering the App component wrapped with the Redux Provider
 root.render(
   <Provider store={store}>
     <App />
